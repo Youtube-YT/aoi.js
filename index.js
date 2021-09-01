c04c26ba62c86347d94ae4855e080039127fbd2d
Youtube-YT <76273241+Youtube-YT@users.noreply.github.com>
2021-09-01 20:26:59 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+ const Aoijs = require("aoi.js")
+const bot = new Aoijs.Bot({
+  sharding: false, //true or false
+  shardAmount: 2, //Shard amount
+  mobile: false, //true or false - Discord Mobile Status
+  //dbhToken: "API KEY", Remove // if using, get an API Key from their Server
+ fetchInvites: true,
+  token: process.env.TOKEN, //Discord Bot Token
+  prefix: ["-"],
+  mobile: false,
+  autoUpdate: ["false"]//Change PREFIX to your Prefix
+})
+
+bot.onMessageUpdate() 
+bot.onMessage(); // Allows Commands to Executed
+bot.loadCommands(`./commands/`);
